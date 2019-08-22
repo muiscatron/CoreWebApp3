f26752eed063c7b848bfa012c458bbdad4daea50
Conor McCarthy <conor.mccarthy@tillerinvest.com>
2019-08-22 15:11:25 +0100

    Initial Commit

---
+++ CoreWebApp3/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CoreWebApp3.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CoreWebApp3.Areas.Identity.IdentityHostingStartup))]
+namespace CoreWebApp3.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
